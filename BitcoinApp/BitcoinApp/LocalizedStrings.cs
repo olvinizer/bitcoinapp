f02713e973e0f70df7d8d5bc4ef84ffd32bc9a0a
olvinizer <nikolay.lapshin@gmail.com>
2013-12-04 20:52:21 +0200

    101 попытка комита на гитхаб

---
+++ BitcoinApp/BitcoinApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using BitcoinApp.Resources;
+
+namespace BitcoinApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
